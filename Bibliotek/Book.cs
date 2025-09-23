630d939d6329bc9e1c84410255ec83306761ed9c
Daniel Aldemir <daniel.aldemir.syne25lin@edu.tucsweden.se>
2025-09-23 18:09:43 +0200

    Add properties and list to Book class
    Updated the `Book` class in the `Bibliotek` namespace to include new properties: `ISBN`, `Title`, `Author`, and `IsAvailable`. Introduced a computed property `available` to indicate the book's availability status. Added a static list of `Book` objects named `Books` to manage multiple instances of the class.

--- Bibliotek/Book.cs
+++ Bibliotek/Book.cs
@@ -8,5 +8,12 @@ namespace Bibliotek
 {
     internal class Book
     {
+        int ISBN;
+        string Title;
+        string Author;
+        bool IsAvailable;
+        string available => IsAvailable ? "Tillgänglig" : "Utlånad";
+
+        public static List<Book> Books = new List<Book>();
     }
 }
